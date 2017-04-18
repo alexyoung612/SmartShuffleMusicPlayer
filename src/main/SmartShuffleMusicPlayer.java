e019e462e042cf1284097602a33bdd6a73232627
alexyoung <alexyoung612@gmail.com>
2017-04-17 21:11:12 -0700

    first commit. setting up git/github

---
+++ src/main/SmartShuffleMusicPlayer.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package main;
+
+/**
+ * @author alexy
+ *
+ */
+public class SmartShuffleMusicPlayer {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
